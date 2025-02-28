14266707722444564a6feebf90391c6273a3814f
Matt Thurling <mthurling@icloud.com>
2025-02-28 19:58:48 +0000

    drf

--- backend/settings.py
+++ backend/settings.py
@@ -37,8 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'api'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer', 
+    ],
+}
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', 
