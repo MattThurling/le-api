8a5dfb641d425b574986a8df99a2e85ba4d096d7
Matt Thurling <mthurling@icloud.com>
2025-02-28 19:54:02 +0000

    json

--- api/views.py
+++ api/views.py
@@ -3,4 +3,4 @@ from rest_framework.decorators import api_view
 
 @api_view(['GET'])
 def get_message(request):
-    return Response({"message": "Hello from Django!"})
+    return Response({"message": "Hello from Django!"}, content_type="application/json")
