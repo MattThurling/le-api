77fb7b99fc0655d574ae9567f58bdea190a003ac
Matt Thurling <mthurling@icloud.com>
2025-03-03 16:08:17 +0000

    storage

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
+from django.conf import settings
 from .models import Prompt
 
 class PromptSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Prompt
-    fields = '__all__'
+    image_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Prompt
+        fields = ["id", "title", "content", "image_url", "created_at"] 
+
+    def get_image_url(self, obj):
+        request = self.context.get("request")
+        if obj.image:
+            return request.build_absolute_uri(obj.image.url) if request else f"{settings.STORAGE_ROOT_URL}{obj.image.url}"
+        return None
